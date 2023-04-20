ecbf131aeddb349f5c22e7165cf4382337494ad3
Dilan Garcia <90223932+Dilan03@users.noreply.github.com>
2023-04-20 15:55:05 -0600

    Diseño BD modificado

--- BD/faundit.sql
+++ BD/faundit.sql
@@ -1,23 +1,27 @@
+CREATE TABLE roles {
+  id INT PRIMARY KEY,
+  rol ENUM('invitado', 'admin', 'usuario')
+};
+
+CREATE TABLE carreras {
+  id VARCHAR(5) PRIMARY KEY,
+  nombre VARCHAR(50)
+};
+
 CREATE TABLE usuarios (
   no_control VARCHAR(12) PRIMARY KEY,
   nombre VARCHAR(50),
   apellido VARCHAR(50),
   contraseña VARCHAR(50),
+  foto MEDIUMBLOB,
   telefono VARCHAR(15),
+  fecha_registro DATETIME,
   id_carrera VARCHAR(5),
-  id_detallesPosts INT,
-  id_comentarios INT,
-  fecha_registro DATETIME
+  id_rol INT,
 
-  FOREIGN KEY (id_carrera) REFERENCES carreras(id_carrera) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  FOREIGN KEY (id_detallesPosts) REFERENCES detallesPosts(id),
-  FOREIGN KEY (id_comentarios) REFERENCES comentarios(id)
+  FOREIGN KEY (id_carrera) REFERENCES carreras(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (id_rol) REFERENCES roles(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
-
-CREATE TABLE carreras {
-    id_carrera VARCHAR(5) PRIMARY KEY,
-    nombre VARCHAR(50)
-};
 
 CREATE TABLE ubicacion (
   id INT PRIMARY KEY,
@@ -25,52 +29,55 @@ CREATE TABLE ubicacion (
   numero VARCHAR(10)
 );
 
-CREATE TABLE etiquetas (
-  id INT PRIMARY KEY,
-  nombre VARCHAR(8) NOT NULL
-);
-
 CREATE TABLE clasificacion (
   id INT PRIMARY KEY,
-  nombre VARCHAR(9) NOT NULL
-);
-
-CREATE TABLE comentarios (
-  id INT PRIMARY KEY,
-  autor_id INT,
-  contenido TEXT, /*Cuánto será el límite?*/
-  fecha_publicacion DATETIME,
-  post_id INT,
+  nombre VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE detallesPosts (
   id INT PRIMARY KEY,
   nombre_objeto VARCHAR(100),
-  descripcion TEXT, /*Cuánto será el límite?*/
-  clasificacion_id INT,
-  autor_id INT,
+  descripcion TINYTEXT, 
   fecha_publicacion DATETIME,
-  img /*tipo de dato de imagen*/,
   id_ubicacion INT,
   id_clasificacion INT,
-  id_etiquetas INT,
 
   FOREIGN KEY (id_ubicacion) REFERENCES ubicacion(id) ON DELETE RESTRICT,
-  FOREIGN KEY (id_clasificacion) REFERENCES clasificacion(id) ON DELETE RESTRICT,
-  FOREIGN KEY (id_etiquetas) REFERENCES etiquetas(id) ON DELETE RESTRICT
+  FOREIGN KEY (id_clasificacion) REFERENCES clasificacion(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE posts {
   id INT PRIMARY KEY,
-  nombre_objeto VARCHAR(100),
-  descripcion TEXT /*cuánto será el límite?*/,
-  clasificacion_id INT,
-  autor_id INT,
-  fecha_publicacion DATETIME,
-  img VARCHAR(255),
+  id_autor INT,
   id_detallesPosts INT,
-  id_comentarios INT,
 
-  FOREIGN KEY (id_detallesPosts) REFERENCES detallesPosts(id) ON DELETE CASCADE ON UPDATE CASCADE
-  FOREIGN KEY (id_comentarios) REFERENCES comentarios(id)
+  FOREIGN KEY (id_detallesPosts) REFERENCES detallesPosts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_autor) REFERENCES usuarios(id) ON DELETE CASCADE ON UPDATE CASCADE
+};
+
+CREATE TABLE etiquetas (
+  id INT PRIMARY KEY,
+  nombre ENUM('ancient', 'lost', 'found', 'gathered'),
+  id_post INT,
+
+  FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE comentarios (
+  id INT PRIMARY KEY,
+  id_autor INT,
+  contenido TEXT, 
+  fecha_publicacion DATETIME,
+  id_post INT,
+
+  FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_autor) REFERENCES usuarios(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE imagenes {
+  id INT PRIMARY KEY,
+  imagen MEDIUMBLOB,
+  id_post INT,
+
+  FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE ON UPDATE CASCADE
 };
