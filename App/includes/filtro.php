d290d57c9b6e1a497a357a3b373bdf18173b83a5
JESR555 <jesus_sierra20@hotmail.com>
2023-05-22 16:22:44 -0600

    LOLO

--- App/includes/filtro.php
+++ App/includes/filtro.php
@@ -3,91 +3,91 @@
     <div class="Clasificación">
         <h1>Clasificación</h1>
         <div>
-            <input type="checkbox" name="clasificacion[]" value="Electronicos" class="check_Electronicos">
+            <input type="checkbox" name="Electronicos" class="check_Electronicos">
             <label for="Electronicos">Electronicos</label>
             <br>
-            <input type="checkbox" name="clasificacion[]" value="Ropa" class="Check_Ropa">
+            <input type="checkbox" name="Ropa" class="Check_Ropa">
             <label for="Ropa">Ropa</label>
             <br>
-            <input type="checkbox" name="clasificacion[]" value="Otros" class="Check_Otros">
+            <input type="checkbox" name="Otros" class="Check_Otros">
             <label for="Otros">Otros</label>
         </div>
     </div>
     <div class="Ubicación_1">
         <h1>Ubicación</h1>
-        <input type="checkbox" name="ubicacion[]" value="F" class="check_F">
+        <input type="checkbox" name="Ed. F" class="check_F">
         <label for="Ed. F">Ed. F</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="G" class="check_G">
+        <input type="checkbox" name="Ed. G" class="check_G">
         <label for="Ed. G">Ed. G</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="Biblioteca" class="check_Biblioteca">
+        <input type="checkbox" name="Biblioteca" class="check_Biblioteca">
         <label for="Biblioteca">Biblioteca</label>
     </div>
     <div class="Ubicación_2">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="F" class="check_F">
+        <input type="checkbox" name="Ed. F" class="check_F">
         <label for="Ed. F">Ed. F</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="G" class="check_G">
+        <input type="checkbox" name="Ed. G" class="check_G">
         <label for="Ed. G">Ed. G</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="Biblioteca" class="check_Biblioteca">
+        <input type="checkbox" name="Gimnasio" class="check_GYM">
         <label for="Gimnasio">Gimnasio</label>
     </div>
     <div class="Ubicación_3">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="D" class="check_D">
+        <input type="checkbox" name="Ed. D" class="check_D">
         <label for="Ed. D">Ed. D</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="S" class="check_S">
+        <input type="checkbox" name="Ed. S" class="check_S">
         <label for="Ed. S">Ed. S</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="A" class="check_A">
+        <input type="checkbox" name="Ed. A" class="check_A">
         <label for="Ed. A">Ed. A</label>
     </div>
     <div class="Ubicación_4">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="B" class="check_B">
+        <input type="checkbox" name="Ed. B" class="check_B">
         <label for="Ed. B">Ed. B</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="P" class="check_P">
+        <input type="checkbox" name="Ed. P" class="check_P">
         <label for="Ed. P">Ed. P</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="M" class="check_M">
+        <input type="checkbox" name="Ed. M" class="check_M">
         <label for="Ed. M">Ed. M</label>
     </div>
     <div class="Ubicación_5">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="X" class="check_X">
+        <input type="checkbox" name="Ed. X" class="check_X">
         <label for="Ed. X">Ed. X</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="Z" class="check_Z">
+        <input type="checkbox" name="Ed. Z" class="check_Z">
         <label for="Ed. Z">Ed. Z</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="W" class="check_W">
+        <input type="checkbox" name="Ed. W" class="check_W">
         <label for="Ed. W">Ed. W</label>
     </div>
     <div class="Ubicación_6">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="Q" class="check_Q">
+        <input type="checkbox" name="Ed. Q" class="check_Q">
         <label for="Ed. Q">Ed. Q</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="L" class="check_L">
+        <input type="checkbox" name="Ed. L" class="check_L">
         <label for="Ed. L">Ed. L</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="Y" class="check_Y">
+        <input type="checkbox" name="Ed. Y" class="check_Y">
         <label for="Ed. Y">Ed. Y</label>
     </div>
     <div class="Ubicación_7">
         <br><br><br><br>
-        <input type="checkbox" name="ubicacion[]" value="I" class="check_I">
+        <input type="checkbox" name="Ed. I" class="check_I">
         <label for="Ed. I">Ed. I</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="U" class="check_U">
+        <input type="checkbox" name="Ed. U" class="check_U">
         <label for="Ed. U">Ed. U</label>
         <br>
-        <input type="checkbox" name="ubicacion[]" value="WW" class="check_WW">
+        <input type="checkbox" name="Ed. WW" class="check_WW">
         <label for="Ed. WW">Ed. WW</label>
     </div>
 </div>
--- App/includes/header.php
+++ App/includes/header.php
@@ -12,37 +12,21 @@
     <div class="container">        
         <header class="header">
             <i class="logo"><a href="index.php"><img src="assets/icons/logo.svg" alt="logo"></a></i>
-            <?php
-            if (isset($_POST['buscador'])) {
-                $buscar = $_POST['buscador'];
-
-                // Escapar caracteres especiales en la cadena de búsqueda
-                $buscar = mysqli_real_escape_string($conn, $buscar);
-
-                // Construir la consulta SQL con la condición de búsqueda
-                $consulta_posts = "
-                SELECT p.id, p.imagen, detalles.nombre_objeto, detalles.fecha_publicacion, clas.nombre,
-                (SELECT nombre FROM etiquetas WHERE etiquetas.nombre = 'ancient' AND etiquetas.id_post = p.id) as ancient,
-                (SELECT nombre FROM etiquetas WHERE etiquetas.nombre = 'lost' AND etiquetas.id_post = p.id) as lost,
-                (SELECT nombre FROM etiquetas WHERE etiquetas.nombre = 'found' AND etiquetas.id_post = p.id) as found,
-                (SELECT nombre FROM etiquetas WHERE etiquetas.nombre = 'gathered' AND etiquetas.id_post = p.id) as gathered
-                FROM posts p
-                INNER JOIN detallesposts detalles ON p.id_detallesPosts = detalles.id
-                INNER JOIN clasificacion clas ON detalles.id_clasificacion = clas.id
-                WHERE detalles.nombre_objeto LIKE '%$buscar%';";
-
-                // Ejecutar la consulta y mostrar los resultados
-                $result_posts = mysqli_query($conn, $consulta_posts);
-                // ...
-                //
-            }
-            ?>
-<div class="search__container">
-    <form class="search__form" method="POST">
-        <button class="button__search" type="submit"> <i> <img src="assets/icons/search.svg"> </i> </button>
-        <input type="text" placeholder="Buscar..." name="buscador">
-    </form>
-</div>
+            <div class="search__container">
+                <?php
+                $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                ?>
+                <?php if (str_contains($actual_link, 'index')) { ?>
+                    <div>
+                        <h2 class="gallery__titulo">Found it</h2>
+                    </div>
+                <?php } else {?>
+                    <form class="search__form">
+                        <button class="button__search" type="submit"> <i> <img src="assets/icons/search.svg"> </i> </button>
+                        <input type="text" placeholder="Buscar...">
+                    </form>
+                <?php } ?>
+            </div>
             <div class="header__options">
                 <?php if(!empty($_SESSION["id"])) { ?>
                     <div class="options__logeduser">
