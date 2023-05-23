f76fe59d743b4946e587246de227aae7229a0d56
Erick_Pérez <gatito.zombie@outlook.es>
2023-05-23 12:06:22 -0600

    no sirve jaja

--- App/includes/tarjeta_slider.php
+++ App/includes/tarjeta_slider.php
@@ -1,28 +1,18 @@
-<?php if (!empty($posts)): ?>
-    <?php foreach ($posts as $post): ?>
-        <div class="tarjeta slider-wh">
-            <div class="tarjeta__etiquetas">
-                <?php if (!empty($post['ancient'])): ?>
-                    <div class="etiquetaEST etiquetaEST--ancient"><span>#Ancient</span></div>
-                <?php endif; ?>
-                <?php if (!empty($post['found'])): ?>
-                    <div class="etiquetaEST etiquetaEST--found"><span>#Found</span></div>
-                <?php endif; ?>
-                <!-- Agregar etiquetas adicionales según sea necesario -->
-            </div>
-            <div class="tarjeta__image">
-                <img src="<?php echo $post['imagen']; ?>" alt="">
-            </div>
-            <div class="tarjeta__detalles">
-                <h4 class="tarjeta__nombre"><?php echo $post['nombre_objeto']; ?></h4>
-                <span>
-                    <p class="etiquetaCAT"><?php echo $post['nombre']; ?></p>
-                    <button class="boton boton__tarjeta"><a href="detailobject.php?id=<?php echo $post['id']; ?>">Informacion</a></button>
-                </span>
-                <span class="tarjeta__fecha">Publicado: <?php echo $post['fecha_publicacion']; ?></span>
-            </div>
-        </div>
-    <?php endforeach; ?>
-<?php else: ?>
-    <p>No se encontraron posts.</p>
-<?php endif; ?>
+<div class="tarjeta slider-wh ">
+    <div class="tarjeta__etiquetas">
+        <div class="etiquetaEST etiquetaEST--ancient"><span>#Ancient</span></div>
+        <div class="etiquetaEST etiquetaEST--found"><span>#Found</span></div>
+        <!-- <div class="etiquetaEST etiquetaEST--gathered"><span>#Gathered</span></div> -->
+    </div>
+    <div class="tarjeta__image">
+        <img src="assets/images/test.jpg" alt="">
+    </div>
+    <div class="tarjeta__detalles">
+        <h4 class="tarjeta__nombre">Objeto 1</h4>
+        <span>
+            <p class="etiquetaCAT">electronicos</p>
+            <button class="boton boton__tarjeta"><a href="detailobject.php">Informacion</a></button>
+        </span>
+        <span class="tarjeta__fecha">Perdido: 22/02/2023</span>
+    </div>
+</div>
