cfbdfa3ce7e1f953ca8383ce6f4e634cb5051239
Erick_Pérez <gatito.zombie@outlook.es>
2023-05-23 12:51:03 -0600

    casi ptm

--- App/index.php
+++ App/index.php
@@ -2,7 +2,6 @@
 require 'functions/conexion.php';
 require 'functions/registrar.php';
 require 'functions/login.php';
-require 'functions/mostrar_reciente.php';
 
 if (!empty($_SESSION["id"])) {
     $id = $_SESSION["id"];
@@ -24,57 +23,98 @@ if (!empty($_SESSION["id"])) {
     <h2 class="RecienteGallery__titulo">Encontrado recientemente</h2>
     <div class="sliderContainer">
         <div class="flechasImagen flechasSlider">
-                    <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
-                    <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
-                 </div>
-                    <div class="tarjetaSlider">
-                    <?php
-                        while($row_posts = mysqli_fetch_array($result_posts_rec)) {
-                            if (($row_posts['found']) == 'found') {
-                            include 'includes/tarjeta_slider.php';
-                        }
-                        }
-            ?>
+            <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
+            <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
         </div>
+        <div class="tarjetaSlider">
+    <?php include_once 'functions/conexion.php'; ?>
+    <?php include_once 'functions/mostrar_encontrado_reciente.php'; ?>
+    <?php foreach ($postsF as $postF) : ?>
+        <div class="tarjeta slider-wh">
+            <div class="tarjeta__etiquetas">
+                <?php if ($postF['found']) : ?>
+                    <div class="etiquetaEST etiquetaEST--found"><span>#Found</span></div>
+                <?php endif; ?>
+            </div>
+            <div class="tarjeta__image">
+                <img src="data:image/jpeg;base64,<?php echo $postF['imagen']; ?>" alt="">
+            </div>
+            <div class="tarjeta__detalles">
+                <h4 class="tarjeta__nombre"><?php echo $postF['nombre_objeto']; ?></h4>
+                <span>
+                    <p class="etiquetaCAT"><?php echo $postF['nombre']; ?></p>
+                    <button class="boton boton__tarjeta"><a href="detail.php">Informacion</a></button>
+                </span>
+                <span class="tarjeta__fecha">Encontrado: <?php echo $postF['fecha_publicacion']; ?></span>
+            </div>
+        </div>
+    <?php endforeach; ?>
 
+</div>
     </div>
 
-    <section class="main RecienteGallery">
     <h2 class="RecienteGallery__titulo">Perdido recientemente</h2>
     <div class="sliderContainer">
         <div class="flechasImagen flechasSlider">
-                    <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
-                    <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
-                 </div>
-                    <div class="tarjetaSlider">
-                    <?php
-                        while($row_posts = mysqli_fetch_array($result_posts_rec)) {
-                            if (($row_posts['lost']) == 'lost') {
-                            include 'includes/tarjeta_gal.php';
-                        }
-                        }
-            ?>
+            <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
+            <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
         </div>
+        <div class="tarjetaSlider">
+    <?php include_once 'functions/conexion.php'; ?>
+    <?php include_once 'functions/mostrar_perdido_reciente.php'; ?>
+    <?php foreach ($postsL as $postL) : ?>
+        <div class="tarjeta slider-wh">
+            <div class="tarjeta__etiquetas">
+                <?php if ($postL['lost']) : ?>
+                    <div class="etiquetaEST etiquetaEST--lost"><span>#Lost</span></div>
+                <?php endif; ?>
+            </div>
+            <div class="tarjeta__image">
+                <img src="data:image/jpeg;base64,<?php echo $postL['imagen']; ?>" alt="">
+            </div>
+            <div class="tarjeta__detalles">
+                <h4 class="tarjeta__nombre"><?php echo $postL['nombre_objeto']; ?></h4>
+                <span>
+                    <p class="etiquetaCAT"><?php echo $postL['nombre']; ?></p>
+                    <button class="boton boton__tarjeta"><a href="detail.php">Informacion</a></button>
+                </span>
+                <span class="tarjeta__fecha">Perdido: <?php echo $postL['fecha_publicacion']; ?></span>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
 
     </div>
 
-    <section class="main RecienteGallery">
     <h2 class="RecienteGallery__titulo">Recuperado recientemente</h2>
     <div class="sliderContainer">
         <div class="flechasImagen flechasSlider">
-                    <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
-                    <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
-                 </div>
-                    <div class="tarjetaSlider">
-                    <?php
-                        while($row_posts = mysqli_fetch_array($result_posts_rec)) {
-                            if (($row_posts['gathered']) == 'gathered') {
-                            include 'includes/tarjeta_slider.php';
-                        }
-                        }
-            ?>
+            <span class="circle"> <img src="assets/icons/flechaizquierda.svg" alt="" class="fleft"></span>
+            <span class="circle"> <img src="assets/icons/flechaderecha.svg" alt="" class="fright"></span>
         </div>
-
+        <div class="tarjetaSlider">
+    <?php include_once 'functions/conexion.php'; ?>
+    <?php include_once 'functions/mostrar_recuperado_reciente.php'; ?>
+    <?php foreach ($postsG as $postG) : ?>
+        <div class="tarjeta slider-wh">
+            <div class="tarjeta__etiquetas">
+                <?php if ($postG['Gathered']) : ?>
+                    <div class="etiquetaEST etiquetaEST--Gathered"><span>#Gathered</span></div>
+                <?php endif; ?>
+            </div>
+            <div class="tarjeta__image">
+                <img src="data:image/jpeg;base64,<?php echo $postG['imagen']; ?>" alt="">
+            </div>
+            <div class="tarjeta__detalles">
+                <h4 class="tarjeta__nombre"><?php echo $postG['nombre_objeto']; ?></h4>
+                <span>
+                    <p class="etiquetaCAT"><?php echo $postG['nombre']; ?></p>
+                    <button class="boton boton__tarjeta"><a href="detail.php">Informacion</a></button>
+                </span>
+                <span class="tarjeta__fecha">Recuperado: <?php echo $postG['fecha_publicacion']; ?></span>
+            </div>
+        </div>
+    <?php endforeach; ?>
     </div>
 </section>
 </div> <!-- div cierre container -->
