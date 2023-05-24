416ad4ca7f1271e22360f6061496d20a9ede03c0
Jesus Arellano <94822020+01Jesus01@users.noreply.github.com>
2023-05-24 11:21:00 -0600

    Nuevo Comentario

--- App/includes/borrar_comentario.php
+++ App/includes/borrar_comentario.php
@@ -1,3 +1,51 @@
+<?php
+    /*$id = $_GET['id'];
+    include("conexion.php");
+
+    //  delete from comentarios where id = $id
+
+    $sql = "delete from comentarios where id = '".$id."'";
+    $resultado = mysqli_query($conexion, $sql);
+
+    if($resultado){
+        echo "<script> alert('Los datos se eliminaron correctamente
+                de la BD');
+                location.assign('detailobject.php');
+            </script>";
+    }else{
+        echo "<script> alert('Los datos No se eliminaron de la BD');
+                location.assign('detailobject.php');
+            </script>";
+    }*/
+
+    // Definir el ID del comentario a eliminar
+    //$id = 1;
+    // Construir la consulta SQL para eliminar el comentario
+    //$sql = "DELETE FROM comentarios WHERE id = $id";
+
+    // Ejecutar la consulta SQL
+    //if ($conn->query($sql) === TRUE) {
+      //  echo "El comentario ha sido eliminado exitosamente.";
+    //} else {
+        //echo "Error al eliminar el comentario: " . $conn->error;
+    //}
+
+// Cerrar la conexión
+//$conn->close();
+if(isset($_POST["Confirmar"])) {   
+        
+    //$comentario = $_POST['comenta'];
+    //$query = "INSERT INTO comentarios(id, id_autor, contenido, fecha_publicacion, id_post) values(1, '21550135', 'hhj', '2023-06-02 00:00:0', 0)";
+    mysqli_query($conn, "DELETE FROM comentarios WHERE id = 3"); 
+    if($result) {
+        echo 'efsf';
+    } else {
+        die(mysql_error());
+
+    }
+}
+?>
+
 <form class="login-form eliminar_post movLR">
     <h2 class="login-titulo">Borrar comentario</h2>
     
--- App/includes/comentario.php
+++ App/includes/comentario.php
@@ -1,3 +1,34 @@
+<?php
+
+    /*if(isset($_POST['enviar'])){
+
+        $comentario = $_POST['comenta'];
+        //  insert into comentarios (id, id_autor, contenido, fecha_publicacion, id_post) 
+        //  values (1, 'L21550135', 'Comentario de prueba', '2023-05-01 00:00:0', 1);
+        $sql = "Insert into comentarios (id, id_autor, contenido, fecha_publicacion, id_post) values ('"$_POST['comenta']."', '"$_SESSION["id"] = $row["no_control"]."', NOW())";
+        header("location: index.php");
+
+    }*/
+
+    if(isset($_POST["enviar"])) {   
+        
+        //$comentario = $_POST['comenta'];
+        //$query = "INSERT INTO comentarios(id, id_autor, contenido, fecha_publicacion, id_post) values(1, '21550135', 'hhj', '2023-06-02 00:00:0', 0)";
+        mysqli_query($conn, "INSERT INTO comentarios(id_autor, contenido, fecha_publicacion, id_post) values('21550135', 'hola', '2023-06-02 00:00:0', 0)"); 
+        if($result) {
+            echo 'efsf';
+        } else {
+            die(mysql_error());
+        }
+    // if($query) { 
+    //     echo "<script> alert('El comentario se ingreso')</script>";
+        
+    // }else{
+    //     echo "<script> alert('El comentario no se ingreso')</script>";
+    // }
+}
+
+?>
 <div class="user_1">
     <img class="options__user hideElement" src="assets/icons/user-box.svg">
     <span>
