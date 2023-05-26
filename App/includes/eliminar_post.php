ec763ca6a1058b3d0b5b9ae142157e5ae8d344e2
JESR555 <jesus_sierra20@hotmail.com>
2023-05-26 12:50:24 -0600

    xd

--- App/includes/eliminar_post.php
+++ App/includes/eliminar_post.php
@@ -1,9 +1,10 @@
-<form class="login-form eliminar_post movLR">
+
+<form class="login-form eliminar_post movLR" method="POST">
     <h2 class="login-titulo">Eliminar publicación</h2>
     
     <div class="eliminar_botones">
-       <button type="submit" class="login-button">Confirmar</button>
-       <button type="submit" class="login-button">Cancelar</button>
+       <button type="submit" class="login-button" name="confirmar">Confirmar</button>
+       <button type="submit" class="login-button" name="cancelar">Cancelar</button>
     </div>
 
     <button class="cerrar"><img src="assets/icons/equis.svg" id="cerrar_modal"></button>
