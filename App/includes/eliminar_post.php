fcd6407f9d5060b54acc1d5b9191c325351fa9be
JESR555 <jesus_sierra20@hotmail.com>
2023-05-27 15:28:45 -0600

    cosas

--- App/includes/eliminar_post.php
+++ App/includes/eliminar_post.php
@@ -1,27 +1,30 @@
 <?php
+// Aquí debes incluir el código de conexión a la base de datos
+$serverName = "127.0.0.1:33065";
+$userName = "found_it";
+$password = "123";
+$dbName = "found_it";
+
 // Verificar si se ha hecho clic en el botón "Confirmar"
 if (isset($_POST["confirmar"])) {
-    // Aquí debes incluir el código de conexión a la base de datos
-    $serverName = "127.0.0.1:33065";
-    $userName = "found_it";
-    $password = "123";
-    $dbName = "found_it";
-
+    
 $conn = mysqli_connect($serverName, $userName, $password, $dbName);
 
     // Obtener el ID de la publicación a eliminar
-    $id_post = $_GET['id_post']; // Asegúrate de pasar el ID de la publicación a través de la URL o ajustar esto según corresponda
+    if(isset($_GET['id'])) {
+        $id_post = $_GET['id'];
+     }
 
     // Eliminar la publicación de la tabla "posts"
-    $queryEliminarPost = "DELETE * FROM posts WHERE id = $id_post";
+    $queryEliminarPost = "DELETE FROM posts WHERE id = $id_post";
     mysqli_query($conn, $queryEliminarPost);
 
     // Eliminar los detalles de la publicación de la tabla "detallesposts"
-    $queryEliminarDetalles = "DELETE * FROM detallesposts WHERE id = $id_post";
+    $queryEliminarDetalles = "DELETE FROM detallesposts WHERE id = $id_post";
     mysqli_query($conn, $queryEliminarDetalles);
 
     // Eliminar las etiquetas de la publicación de la tabla "etiquetas"
-    $queryEliminarEtiquetas = "DELETE FROM * etiquetas WHERE id_post = $id_post";
+    $queryEliminarEtiquetas = "DELETE FROM  etiquetas WHERE id_post = $id_post";
     mysqli_query($conn, $queryEliminarEtiquetas);
 
     // Redirigir al usuario a la página correspondiente según el estatus de la publicación
@@ -36,6 +39,7 @@ $conn = mysqli_connect($serverName, $userName, $password, $dbName);
     header("Location: ../index.php"); // Ejemplo: redirige al usuario a la página principal
 }
 ?>
+
 <form class="login-form eliminar_post movLR" method="POST">
     <h2 class="login-titulo">Eliminar publicación</h2>
     
