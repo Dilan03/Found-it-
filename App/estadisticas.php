66075baa28c3a42250c9a2ce3a67bf5abdf46579
JESR555 <jesus_sierra20@hotmail.com>
2023-05-31 13:07:24 -0600

    PROCEDIMIENTO ALMACENADOs
    HOLA A TODOS HELLO EVERYONE

--- App/estadisticas.php
+++ App/estadisticas.php
@@ -13,21 +13,53 @@ if (!empty($_SESSION["id"])) {
 
 }
 
-// Verificar si se ha hecho clic en el botón "Mostrar cantidad de posts"
-if (isset($_POST['mostrar_cantidad'])) {
-    // Llamar al procedimiento almacenado para contar los posts
-    $sql = "CALL contar_posts(@cant)";
-    $result = mysqli_query($conn, $sql);
+// Llamar al procedimiento almacenado para contar los posts
+$sql = "CALL contar_posts(@cant_posts)";
+$result = mysqli_query($conn, $sql);
 
-    // Recuperar el valor del parámetro OUT
-    $sql = "SELECT @cant AS cant";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    $cantidadPosts = $row['cant'];
+// Recuperar el valor del parámetro OUT
+$sql = "SELECT @cant_posts AS cant_posts";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$cantidadPosts = $row['cant_posts'];
 
-    // Guardar la cantidad de posts en el almacenamiento local del navegador
-    echo "<script>localStorage.setItem('cantidadPosts', $cantidadPosts);</script>";
-}
+// Llamar al procedimiento almacenado para contar los objetos encontrados
+$sql = "CALL contar_objetos_encontrados(@cant_encontrados)";
+$result = mysqli_query($conn, $sql);
+
+// Recuperar el valor del parámetro OUT
+$sql = "SELECT @cant_encontrados AS cant_encontrados";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$cantidadEncontrados = $row['cant_encontrados'];
+
+// Llamar al procedimiento almacenado para contar los objetos perdidos
+$sql = "CALL contar_objetos_perdidos(@cant_perdidos)";
+$result = mysqli_query($conn, $sql);
+
+// Recuperar el valor del parámetro OUT
+$sql = "SELECT @cant_perdidos AS cant_perdidos";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$cantidadPerdidos = $row['cant_perdidos'];
+
+// Llamar al procedimiento almacenado para contar los objetos recuperados
+$sql = "CALL contar_objetos_recuperados(@cant_recuperados)";
+$result = mysqli_query($conn, $sql);
+
+// Recuperar el valor del parámetro OUT
+$sql = "SELECT @cant_recuperados AS cant_recuperados";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$cantidadRecuperados = $row['cant_recuperados'];
+
+// Guardar las cantidades en el almacenamiento local del navegador
+echo "<script>
+localStorage.setItem('cantidadPosts', $cantidadPosts);
+localStorage.setItem('cantidadEncontrados', $cantidadEncontrados);
+localStorage.setItem('cantidadPerdidos', $cantidadPerdidos);
+localStorage.setItem('cantidadRecuperados', $cantidadRecuperados);
+</script>";
 ?>
 
 <?php include_once 'includes/header.php' ?>
@@ -36,20 +68,30 @@ if (isset($_POST['mostrar_cantidad'])) {
 <section class="main main__gallery">
     <div class="gallery__top">
         <h1 class="gallery__titulo">Estadisticas</h1>
-        <!-- Agregar el botón "Mostrar cantidad de posts" -->
-        <form action="" method="post">
-            <input type="submit" name="mostrar_cantidad" value="Mostrar cantidad de posts">
-        </form>
-        <!-- Mostrar la cantidad de posts guardada en la tabla dinámica -->
+        <!-- Mostrar la cantidad de posts, objetos encontrados, objetos perdidos y objetos recuperados en la tabla dinámica -->
         <table id="tablaEstadisticas">
             <thead>
                 <tr>
-                    <th>Cantidad de posts</th>
+                    <th>Categoría</th>
+                    <th>Cantidad</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
+                    <td>Posts</td>
                     <td id="cantidadPosts"></td>
+                </tr>
+                <tr>
+                    <td>Objetos encontrados</td>
+                    <td id="cantidadEncontrados"></td>
+                </tr>
+                <tr>
+                    <td>Objetos perdidos</td>
+                    <td id="cantidadPerdidos"></td>
+                </tr>
+                <tr>
+                    <td>Objetos recuperados</td>
+                    <td id="cantidadRecuperados"></td>
                 </tr>
             </tbody>
         </table>
@@ -63,9 +105,16 @@ if (isset($_POST['mostrar_cantidad'])) {
 <?php include_once 'includes/footer.php' ?>
 
 <script>
-    // Obtener la cantidad de posts del almacenamiento local y mostrarla en la tabla
+    // Obtener las cantidades del almacenamiento local y mostrarlas en la tabla
     var cantidadPosts = localStorage.getItem('cantidadPosts');
-    if (cantidadPosts) {
+    var cantidadEncontrados = localStorage.getItem('cantidadEncontrados');
+    var cantidadPerdidos = localStorage.getItem('cantidadPerdidos');
+    var cantidadRecuperados = localStorage.getItem('cantidadRecuperados');
+
+    if (cantidadPosts && cantidadEncontrados && cantidadPerdidos && cantidadRecuperados) {
         document.getElementById('cantidadPosts').textContent = cantidadPosts;
+        document.getElementById('cantidadEncontrados').textContent = cantidadEncontrados;
+        document.getElementById('cantidadPerdidos').textContent = cantidadPerdidos;
+        document.getElementById('cantidadRecuperados').textContent = cantidadRecuperados;
     }
 </script>
